56c070b22aa11de4b0e48d4132826bff8ced8183
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-26 21:47:17 +0700

    add constraint null false to event, locaation

--- db/migrate/20180926141739_create_locations.rb
+++ db/migrate/20180926141739_create_locations.rb
@@ -2,12 +2,12 @@ class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
       t.references :event, foreign_key: true
-      t.text :address
-      t.string :city
-      t.string :state
-      t.string :country
-      t.string :postal_code
-      t.string :phone_number
+      t.text :address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :country, null: false
+      t.string :postal_code, null: false
+      t.string :phone_number, null: false
 
       t.timestamps
     end
