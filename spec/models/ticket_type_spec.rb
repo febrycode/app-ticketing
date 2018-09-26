cf4bdca1c0fbf7ca72af5fad7cd73322bb3b47b8
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-26 22:04:49 +0700

    ticket type belongs to event

--- spec/models/ticket_type_spec.rb
+++ spec/models/ticket_type_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TicketType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:event) }
 end
