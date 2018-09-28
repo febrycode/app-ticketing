574c13e82ca76f3802e5fa66a875df6c64581187
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-28 10:14:26 +0700

    implement validation field event, location

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   belongs_to :location
   has_one :schedule
   has_many :ticket_types
+
+  validates :name, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,11 @@
 class Location < ApplicationRecord
   # Association
   has_many :event
+
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :postal_code, presence: true
+  validates :phone_number, presence: true
 end
