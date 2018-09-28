12a5796672cfeff1f3b3ed32290b04b0a6e151ae
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-28 08:02:12 +0700

    bugfix event should exactly one location

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   # Association test
-  it { should have_one(:location) }
+  it { should belong_to(:location) }
   it { should have_one(:schedule) }
   it { should have_many(:ticket_types) }
 end
