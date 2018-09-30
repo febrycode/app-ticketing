7bfafcaef7252584faf49714927739859cb7a5da
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-30 22:09:59 +0700

    spec for get info transaction

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Transactions API' do
-  let(:headers) { {'Accept': 'application/json'} }
+  describe 'GET /transaction/get_info' do
+    let!(:transaction) { create(:transaction) }
+    let!(:transaction_detail) { create(:transaction_detail, transaction_id: transaction.id) }
 
-  let!(:events) { create_list(:event, 2) }
-  let!(:ticket_types) { create_list(:ticket_type, 2) }
-  let(:transaction_params) do
-    {
-      customer_name: "John Doe",
-      customer_email: "john.doe@test.com",
-      event_id: events.first.id,
-      ticket_types: [
-        {
-          id: ticket_types.first.id,
-          qty: 1
-        }
-      ]
-    }
+    before { get '/transaction/get_info', headers: { 'Accept': 'application/json' } }
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns data transactions' do
+      expect(json['transactions'].length).to eql(1)
+    end
   end
 
   describe 'POST /transaction/purchase' do
+
+    let(:headers) { {'Accept': 'application/json'} }
+
+    let!(:events) { create_list(:event, 2) }
+    let!(:ticket_types) { create_list(:ticket_type, 2) }
+    let(:transaction_params) do
+      {
+        customer_name: "John Doe",
+        customer_email: "john.doe@test.com",
+        event_id: events.first.id,
+        ticket_types: [
+          {
+            id: ticket_types.first.id,
+            qty: 1
+          }
+        ]
+      }
+    end
 
     context 'when purchase an event one time' do
       before { post '/transaction/purchase', params: transaction_params, headers: headers }
